386caf728923ed0d722d8f1b91788bd346b92ea9
Deatheragenator <deatheragenator@gmail.com>
2019-03-08 00:52:00 -0500

    Update AdventureGame.cs

--- AdventureGame.cs
+++ AdventureGame.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,7 +9,7 @@ public class AdventureGame : MonoBehaviour
     [SerializeField] Text textComponent;
     [SerializeField] State startingState;
 
-    
+
 
 
     State state;
@@ -26,59 +26,51 @@ public class AdventureGame : MonoBehaviour
     {
         ManageState();
     }
-    
+
+
+    private void ArraySize(int Input) 
+    {
+        var nextStates = state.GetnextStates();
+        if (Input < nextStates.Length)
+        {
+            state = nextStates[Input];
+        }
+    }
+
     //player controles/input
     private void ManageState()
     {
-        var nextStates = state.GetnextStates();
-        if (Input.GetKeyDown(KeyCode.Alpha1))
+            var nextStates = state.GetnextStates();
 
-        {
-            state = nextStates[0];
-        }
+         {
+                
+                if (Input.GetKeyDown(KeyCode.Alpha1) || Input.GetKeyDown(KeyCode.Keypad1))
 
-        else if (Input.GetKeyDown(KeyCode.Keypad1))
+                {
+                    ArraySize(0);
+                }
 
-        {
-            state = nextStates[0];
-        }
+            else if (Input.GetKeyDown(KeyCode.Alpha2) || Input.GetKeyDown(KeyCode.Keypad2))
+                {
+                    ArraySize(1);
+                }
+                
 
+           
+            else if (Input.GetKeyDown(KeyCode.Alpha3) || Input.GetKeyDown(KeyCode.Keypad3))
+                {
+                    ArraySize(2);
+                }
 
-        else if (Input.GetKeyDown(KeyCode.Alpha2))
-        {
-            state = nextStates[1];
-        }
+            else if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Keypad0))
+                {
+                    ArraySize(3);
+                }   
 
-        else if (Input.GetKeyDown(KeyCode.Keypad2))
+         }
+           
 
-        {
-            state = nextStates[1];
-        }
-
-
-        else if (Input.GetKeyDown(KeyCode.Alpha3))
-        {
-            state = nextStates[2];
-        }
-
-        else if (Input.GetKeyDown(KeyCode.Keypad3))
-        {
-            state = nextStates[2];
-        }
-
-
-        else if (Input.GetKeyDown(KeyCode.Space))
-        {
-            state = nextStates[3];
-        }
-
-        else if (Input.GetKeyDown(KeyCode.Keypad0))
-        {
-            state = nextStates[3];
-        }
-
-
-        textComponent.text = state.GetStateStory();
+            textComponent.text = state.GetStateStory();
 
     }
 }
